2a8dc1eece1b38b2fc6567b7a906f882bfa4f86a
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-19 19:58:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    // Array destructuring with the arguments above ^^
+    // console.log('~~~this arr: ' + this)
+    return {
+        // firstName: firstName,
+        // familyName: familyName,
+        // title: title,
+        // payPerHour: payPerHour,
+        // timeInEvents: [],
+        // timeOutEvents: []
+
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrOfArrs){
+    // return arrOfArrs.map(arr => createEmployeeRecord(arr))
+    // Can be simplified to:
+    return arrOfArrs.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(timeStamp){
+    let newbpRecord = this
+    const dateObj = {
+        type: 'TimeIn',
+        hour: parseInt(timeStamp.substring(11)),
+        date: timeStamp.substring(0, 10)
+    }
+    newbpRecord.timeInEvents.push(dateObj)
+    // console.log('~~~'+JSON.stringify(newbpRecord, null, 2))
+    // console.log('***'+JSON.stringify(newbpRecord.timeInEvents[0], null, 2))
+    return newbpRecord
+}
+
+function createTimeOutEvent(timeStamp){
+    let newbpRecord = this
+    const dateObj = {
+        type: 'TimeOut',
+        hour: parseInt(timeStamp.substring(11)),
+        date: timeStamp.substring(0, 10)
+    }
+    newbpRecord.timeOutEvents.push(dateObj)
+    return newbpRecord
+}
+
+function hoursWorkedOnDate(date){
+    // console.log('~~~'+JSON.stringify(this, null, 2))
+    // console.log(date)
+    const timeInIndex = this.timeInEvents.findIndex(obj => obj.date === date)
+    const timeOutIndex = this.timeOutEvents.findIndex(obj => obj.date === date)
+    let hours = this.timeOutEvents[timeOutIndex].hour - this.timeInEvents[timeInIndex].hour
+    hours = parseInt(hours.toString().replace(/([0])+/g, ''))
+    return hours
+}
+
+function wagesEarnedOnDate(timeStamp){
+    return hoursWorkedOnDate.call(this, timeStamp) * this.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +83,15 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(emp => emp.firstName === firstName)
+
+}
+
+function calculatePayroll(empArr){
+    console.log(JSON.stringify(empArr, null, 2))
+    // console.log(JSON.stringify(empArr[0], null, 2))
+    // console.log('~~~'+allWagesFor.call(empArr[0]))
+
+    return empArr.reduce((pVal, cVal) => allWagesFor.call(cVal) + pVal, 0)
+}
